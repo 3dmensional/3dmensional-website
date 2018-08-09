1565e0fb58204451d281d618c8112289f8c3fbdc
José Daniel <josedanq100@gmail.com>
2018-08-09 15:31:17 -0400

    Configuring Vue and create the standard navbar

---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+
+    <nav class="d-flex">
+      <figure>
+        <a href="#">
+          <Logo class="negative"></Logo>
+        </a>
+
+      </figure>
+      <div class="menu">
+
+        <i class="icon icon-menu"></i>
+        <ul class="d-flex">
+          <li class="d-flex"><a class="nav-link" href="#">Home</a></li>
+          <li class="d-flex"><a class="nav-link" href="#About">About Us</a></li>
+          <li class="d-flex"><a class="nav-link" href="#Services">Services</a></li>
+          <li class="d-flex"><a class="nav-link" href="#Contact">Contact</a></li>
+        </ul>
+      </div>
+
+    </nav>
+
+    <div class="content">
+      <!-- Content page -->
+    </div>
+
+  </main>
+</template>
+
+<script>
+  import Logo from '../assets/logo-neg.svg';
+
+  export default {
+    name: 'Main',
+    components: {
+      Logo
+    }
+  };
+</script>
